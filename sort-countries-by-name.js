04d600be18bca742369eedf24d5b8b524fdcfd6b
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-11-23 00:47:26 +0300

    build UI

--- sort-countries-by-name.js
+++
@@ -1,9 +0,0 @@
-const data = require('./db.json')
-
-const sortCountriesByName = (countries) => {
-    let sorted = countries.sort()
-    return sorted
-}
-
-//testing by printing to console
-console.log(sortCountriesByName(data))
--- sort-countries-by-population.js
+++
@@ -1,8 +0,0 @@
-const data = require('./db.json')
-
-const sortCountriesByPopulation = (countries) => {
-    return countries.sort((a, b) => a.population - b.population)
-}
-    
-//testing by printing to console
-console.log(sortCountriesByPopulation(data))
